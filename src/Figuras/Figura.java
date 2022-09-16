f6accce55ba9bb02421005d725b314d9fd76641f
yosig <ygonzalezm023@alumno.uaemex.mx>
2022-09-16 12:44:29 -0500

    :)

---
+++ src/Figuras/Figura.java
@@ -0,0 +1,12 @@
+package Figuras;
+
+public abstract class Figura {
+    private int lados;
+    public abstract double calcularArea();
+    public abstract double calcularPerimetro();
+
+    @Override
+    public String toString() {
+        return "Figura{}";
+    }
+}
